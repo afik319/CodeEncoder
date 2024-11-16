74fefcf52fe18848bd718aa3cbd097678b40098d
Afik <134519780+afik319@users.noreply.github.com>
2024-11-16 22:00:20 +0200

    Update clone and decode.bat

--- clone and decode.bat
+++ clone and decode.bat
@@ -1,9 +1,7 @@
 @echo off
-git clone https://github.com/afik319/CodeEncoder.git
+git clone https://github.com/afik319/CodeEncoder.git temp_folder
 xcopy temp_folder\* . /E /H /Y
 rmdir /S /Q temp_folder
-
 if not exist venv call venv.bat
 if exist venv call venv\Scripts\activate
-
 python decode.py
